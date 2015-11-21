5db5a6f564914bad7f5b3ff568f42927994779f9
Kriti Sehgal <kritishgl1 @gmail.com>
2015-11-20 21:26:43 -0500

    fnsh touches

--- catFun.py
+++ catFun.py
@@ -1,33 +1,14 @@
 import runWorld as rw
 import drawWorld as dw
 import pygame as pg
+import pygame, sys
+from pygame.locals import *
 from random import randint
 from numpy import absolute
 
+
 ################################################################
 
-# This program is an interactive simulation/game. A cat starts
-# to move across the screen. The direction of movement is reversed
-# on each "mouse down" event.
-#
-# The state of the cat is represented by a tuple (pos, delta-pos).
-# The first element, pos, represents the x-coordinate of the cat.
-# The second element, delta-pos, represents the amount that the
-# position changes on each iteration of the simulation loop.
-#
-# For example, the tuple (7,1) would represent the cat at x-coord,
-# 7, and moving to the right by 1 pixel per "clock tick."
-#
-# The initial state of the cat in this program is (0,1), meaning that the cat
-# starts at the left of the screen and moves right one pixel per tick.
-#
-# Pressing a mouse button down while this simulation run updates the cat state
-# by leaving pos unchanged but reversing delta-pos (changing 1 to -1 and vice
-# versa). That is, pressing a mouse key reverses the direction of the
-# cat.
-#
-# The simulation ends when the cat is allowed to reach either the left
-# or the right edge of the screen.
 
 ################################################################
 
@@ -35,6 +16,28 @@ from numpy import absolute
 name = "Hoo will win? Press the mouse (but not too fast)!"
 width = 1000
 height = 1000
+
+pygame.init()
+
+pygame.display.set_caption('font example')
+size = [640, 480]
+screen = pygame.display.set_mode(size)
+
+clock = pygame.time.Clock()
+
+basicfont = pygame.font.SysFont(None, 48)
+#text
+text = basicfont.render('Hello World!', True, (255, 0, 0), (255, 255, 255))
+textrect = text.get_rect()
+textrect.centerx = screen.get_rect().centerx
+textrect.centery = screen.get_rect().centery
+screen.fill((255, 255, 255))
+screen.blit(text, textrect)
+
+pygame.display.update()
+
+
+
 rw.newDisplay(width, height, name)
 
 ################################################################
@@ -42,66 +45,69 @@ rw.newDisplay(width, height, name)
 # Display the state by drawing a cat at that x coordinate
 myimage = dw.loadImage("cavman.jpg")
 myimage1 = dw.loadImage("hokie.gif")
+
 # state -> image (IO)
 # draw the cat halfway up the screen (height/2) and at the x
 # coordinate given by the first component of the state tuple
-#
+
+
 def updateDisplay(state):
-    dw.fill(dw.black)
+    dw.fill(dw.blue)
     dw.draw(myimage, (state[0], state[2]))
     dw.draw(myimage1, (state[4], state[6]))
 
+
 ################################################################
 
-# Change pos by delta-pos, leaving delta-pos unchanged
-# Note that pos is accessed as state[0], and delta-pos
-# as state[1]. Later on we'll see how to access state
-# components by name (as we saw with records in Idris).
-#
 # state -> state
+
+
 def updateState(state):
     return((state[0] + state[1], state[1], state[2] + state[3], state[3], state[4] + state[5], state[5], state[5] + state[6]))
 
 ################################################################
 
-# Terminate the simulation when the x coord reaches the screen edge,
-# that is, when pos is less then zero or greater than the screen width
-# state -> bool
+
+def updateBound(state):
+    if (state[0] and state[4] >= 1000):
+        return True
+    else:
+        return False
+
+####################################################################
+# Terminate the simulation when the 2 images touch
+
+
 def endState(state):
 
-    if( absolute(state[0] - state[4]) <  150 + 93 and
-       absolute(state[2] - state[6]) <  150 + 115):
+    if(absolute(state[0] - state[4]) < 150 + 93 and
+       absolute(state[2] - state[6]) < 150 + 115):
         return True
     else:
         return False
 
 
+
 ################################################################
 
-# We handle each event by printing (a serialized version of) it on the console
-# and by then responding to the event. If the event is not a "mouse button down
-# event" we ignore it by just returning the current state unchanged. Otherwise
-# we return a new state, with pos the same as in the original state, but
-# delta-pos reversed: if the cat was moving right, we update delta-pos so that
-# it moves left, and vice versa. Each mouse down event changes the cat
-# direction. The game is to keep the cat alive by not letting it run off the
-# edge of the screen.
-#
-# state -> event -> state
-#
+#handle each event by printing it on the console and by then responding to the event
 def handleEvent(state, event):
-#    print("Handling event: " + str(event))
     if (event.type == pg.MOUSEBUTTONDOWN):
         return((state[0], randint(-5, 5), state[2], randint(-5, 5), state[4], randint(-5, 5), state[6], randint(-5, 5)))
     else:
+
         return(state)
 
 ################################################################
 
+
+#############################################################################
+
+
 # World state will be single x coordinate at left edge of world
 
 # The cat starts at the left, moving right
-initState = (randint(0, 499), randint(1, 5), 100, randint(1, 5), 500, randint (1, 5), height/2, randint(1, 5))
+initState =(randint(0, 499), randint(1, 5), 100, randint(1, 5), 500, randint (1, 5), height/2, randint(1, 5))
 
 
 
